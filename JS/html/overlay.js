afbd2eec72bbdde34d984e72729dce09898956a2
VhArno <arnovanhee@gmail.com>
2023-04-25 16:08:27 +0200

    style.css change
    background image add
    color change
    javascript file add

---
+++ JS/html/overlay.js
@@ -0,0 +1,10 @@
+("use strict");
+(function () {
+    let btnInfo = document.querySelector(".info");
+
+    btnInfo.addEventListener('Click', function() {
+        //document.getElementById("info-overlay").style.display = "block";
+        //document.getElementById("info-overlay").style.display = "none";
+        alert("Hello world");
+    })
+})();
