7377608bef3314490b908c551c752f93fe77ba97
VhArno <arnovanhee@gmail.com>
2023-04-25 16:54:23 +0200

    overlay add

--- JS/html/overlay.js
+++ JS/html/overlay.js
@@ -1,10 +1,18 @@
 ("use strict");
 (function () {
-    let btnInfo = document.querySelector(".info");
+    let btnInfo = document.querySelector("#info");
+    let overlay = document.querySelector("#info-overlay");
 
-    btnInfo.addEventListener('Click', function() {
-        //document.getElementById("info-overlay").style.display = "block";
-        //document.getElementById("info-overlay").style.display = "none";
-        alert("Hello world");
+    btnInfo.addEventListener("click", function() {
+        if(overlay.style.display = "none") {
+            overlay.style.display = "block";
+        }
+        if(overlay.style.display != "none") {
+            overlay.style.display = "block";
+        }
+    })
+
+    overlay.addEventListener("click", function() {
+        overlay.style.display = "none";
     })
 })();
