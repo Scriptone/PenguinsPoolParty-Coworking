8ac338749bcb8df3d8633cba122bcca6749bfaf0
= <arne.haers@student.odisee.be>
2023-04-30 18:02:51 +0200

    Password restrictions

--- JS/classes/FormValidator.js
+++ JS/classes/FormValidator.js
@@ -163,6 +163,12 @@
 	});
 
 	formValidator.addValidator({
+		name: "password",
+		method: (field) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+		message: `Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter and one number`,
+	});
+
+	formValidator.addValidator({
 		name: "confirm-password",
 		method: (field) => field.value.trim().length > 0,
 		message: "Confirm password is a required field and was not filled in",
